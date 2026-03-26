faa96e6f40db2aee1c7ed9c1bbacb2cfba4342ce
Ramon Dantas <ramonpolicarpo36@sempreceub.com>
2026-03-25 21:42:00 -0300

    Vue CLI
    Criando um Projeto Vue via prompt de comando e criando o primeiro componente

---
+++ ramon_dantas/aulas/vue_cli/src/components/HomeComponent.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <footer>
+        <p>Bem Vindo! {{ nome }} Olá Mundo do Vue</p>
+    </footer>
+  </div>
+</template>
+
+
+
+<script>
+
+    export default {
+        name: 'HomeComponent',
+        data() {
+            return {
+                nome: 'Ramon'
+            }
+        },
+
+        components: {
+        }
+
+    }
+</script>
+
+<style>
+</style>
