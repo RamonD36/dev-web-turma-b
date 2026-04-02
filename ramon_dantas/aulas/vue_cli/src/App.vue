9fa4768a6d312ac935bcd04d6e1df793bd53c1fa
Ramon Dantas <ramonpolicarpo36@sempreceub.com>
2026-04-01 21:36:56 -0300

    Ciclo de vida e Diretivos Vue
    Add UsuarioComponent
    Add EstiloComponent

--- ramon_dantas/aulas/vue_cli/src/App.vue
+++ ramon_dantas/aulas/vue_cli/src/App.vue
@@ -1,17 +1,26 @@
 <template>
   <div>
     <img alt="Vue logo" src="./assets/logo.png" />
+    <h1>Meu Primeiro APP Vue</h1>
     <HomeComponent />
+    <hr>
+    <UsuarioComponent />
+    <hr>
+    <EstiloComponente />
   </div>
 </template>
 
 <script>
 import HomeComponent from "./components/HomeComponent.vue";
+import UsuarioComponent from "./components/UsuarioComponent.vue";
+import EstiloComponente from "./components/EstiloComponente.vue";
 
 export default {
   name: "App",
   components: {
     HomeComponent,
+    UsuarioComponent,
+    EstiloComponente,
   },
   created() {
     console.log("Created");
