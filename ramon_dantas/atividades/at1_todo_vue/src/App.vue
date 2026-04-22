db2d9510c72c1395ecdca5df58c37074e3e0768e
Ramon Dantas Policarpo <ramonpolicarpo36@gmail.com>
2026-04-22 17:38:32 -0300

    wtf tá tudo errado
    vou corrigir depois

---
+++ ramon_dantas/atividades/at1_todo_vue/src/App.vue
@@ -0,0 +1,85 @@
+<template>
+  <div>
+    <div class="section-1">
+      <h1>Tarefas</h1>
+      <div class="input-group">
+        <input class="input-task" type="text" />
+        <button class="btn-add-task"><i class="fa-solid fa-plus"></i></button>
+      </div>
+    </div>
+    <ul class="section-2">
+      <li v-for="task in tasks" :key="">
+        
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+import { ref } from "vue";
+import TaskComponent from "./components/TaskComponent.vue";
+const tasks = ref([
+  <TaskComponent />
+])
+
+
+export default {
+  name: "App",
+  components: {
+    TaskComponent,
+  },
+};
+</script>
+
+<style>
+.section-1 {
+  margin: 5% 25% 0 25%;
+  display: flex;
+  flex-direction: column;
+  text-align: start;
+  border: solid 2px #2c3e50;
+  border-radius: 1em;
+  padding: 1em;
+}
+
+.section-2 {
+    margin: 1em 25%;
+}
+
+.section-1 h1 {
+  margin: 0;
+  margin-bottom: 0.5em;
+}
+
+.input-group {
+  flex: 1;
+  display: flex;
+  flex-direction: row;
+}
+
+.input-task {
+  font-size: 1em;
+  padding: 0.5em 0.7em 0.5em 0.7em;
+  flex: 1;
+  margin-right: 0.5em;
+  border: solid 2px #2c3e50;
+  border-radius: 50vh;
+}
+.btn-add-task {
+  color: azure;
+  background-color: #1ca046;
+  width: 40px;
+  height: 40px;
+  font-size: 1em;
+  padding: 0.5em;
+  border: none;
+  border-radius: 50vh;
+}
+
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  color: #2c3e50;
+}
+</style>
---
+++ ramon_dantas/atividades/at1_todo_vue/src/components/TaskComponent.vue
@@ -0,0 +1,58 @@
+<template>
+    <div @click="markCompleted" class="task" :class="{'task-complete' : isComplete}">
+        <p>{{ taskDescripton }}</p>
+        <button><i class="fa-solid fa-xmark"></i></button>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "TaskComponent",
+    data() {
+        return {
+            taskDescripton: "Descrição da tarefa",
+            isComplete: false,
+        };
+    },
+
+    methods: {
+        markCompleted() {
+            this.isComplete = !this.isComplete;
+}
+
+    },
+};
+
+
+</script>
+
+<style>
+:scope .task {
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+    border: solid 2px #2c3e50;
+    border-radius: 1em;
+    padding: 1em;
+}
+
+.task-complete {
+    text-decoration: line-through;
+    opacity: 50%;
+}
+
+.task p {
+    margin: 0;
+}
+
+.task button {
+    color: azure;
+    background-color: #cc3a3a;
+    margin: 0;
+    margin-left: 0.5em;
+    width: 2em;
+    height: 2em;
+    border: none;
+    border-radius: 5vh;
+}
+</style>
